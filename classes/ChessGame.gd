11c6559b113c13a6628dbf3e166fec227c5a74a6
Andrew Ribeiro <andrewnetwork@gmail.com>
2025-08-14 03:22:32 -0400

    Knight move logic

--- classes/ChessGame.gd
+++ classes/ChessGame.gd
@@ -32,66 +32,70 @@ func get_available_moves(piece: ChessPiece):
 	var starting_row = WHITE_STARTING_ROW if piece.color == ChessPiece.Side.WHITE else BLACK_STARTING_ROW
 	var opponent_side = ChessPiece.Side.BLACK if piece.color == ChessPiece.Side.WHITE else ChessPiece.Side.WHITE
 	
-	if piece.type == ChessPiece.Type.PAWN: 
-		# One forward.
-		move = piece.location+Vector2i(1,0)*flip
-		if is_move_within_board(move) and BOARD(move) == EMPTY_SQUARE:
-			moves.append(move)
-		# Two forward, from starting row.
-		move = piece.location+Vector2i(2,0)*flip
-		if is_move_within_board(move) and piece.location.x == starting_row and BOARD(move) == EMPTY_SQUARE:
-			moves.append(move)
-		# Left diagonal.
-		move = piece.location+Vector2i(1,1)*flip
-		if is_move_within_board(move) and BOARD(move) != EMPTY_SQUARE and BOARD(move).color == opponent_side:
-			moves.append(move)
-		# Right diagonal.
-		move = piece.location+Vector2i(1,-1)*flip
-		if is_move_within_board(move) and BOARD(move) != EMPTY_SQUARE and BOARD(move).color == opponent_side:
-			moves.append(move)
-	elif piece.type == ChessPiece.Type.ROOK:
-		# Column scan. 
-		#var take_step = func(a, b): for idx in len(a): a[idx] = a[idx]+b[idx]
-		var basis_vectors = [Vector2i(1,0),Vector2i(-1,0),Vector2i(0,1), Vector2i(0,-1)]
-		var locations = []
-		locations.resize(4)
-		locations.fill(piece.location)
-	
-		var idx = 0
-		while len(basis_vectors) > 0:
-			locations[idx] += basis_vectors[idx]
-			if is_move_within_board(locations[idx]):
-				if BOARD(locations[idx]) == EMPTY_SQUARE:
-					moves.append(locations[idx])
-				elif BOARD(locations[idx]).color == opponent_side:
-					moves.append(locations[idx])
-					# Done searching in this direction.
-					basis_vectors.remove_at(idx)
-					locations.remove_at(idx)
-					# Removing an element shrinks the array, push the index back by one. 
-					idx -= 1
+	match piece.type:
+		ChessPiece.Type.PAWN: 
+			# One forward.
+			move = piece.location+Vector2i(1,0)*flip
+			if is_move_within_board(move) and BOARD(move) == EMPTY_SQUARE:
+				moves.append(move)
+			# Two forward, from starting row.
+			move = piece.location+Vector2i(2,0)*flip
+			if is_move_within_board(move) and piece.location.x == starting_row and BOARD(move) == EMPTY_SQUARE:
+				moves.append(move)
+			# Left diagonal.
+			move = piece.location+Vector2i(1,1)*flip
+			if is_move_within_board(move) and BOARD(move) != EMPTY_SQUARE and BOARD(move).color == opponent_side:
+				moves.append(move)
+			# Right diagonal.
+			move = piece.location+Vector2i(1,-1)*flip
+			if is_move_within_board(move) and BOARD(move) != EMPTY_SQUARE and BOARD(move).color == opponent_side:
+				moves.append(move)
+		ChessPiece.Type.ROOK:
+			var basis_vectors = [Vector2i(1,0),Vector2i(-1,0),Vector2i(0,1), Vector2i(0,-1)]
+			var locations = []
+			locations.resize(4)
+			locations.fill(piece.location)
+		
+			var idx = 0
+			while len(basis_vectors) > 0:
+				locations[idx] += basis_vectors[idx]
+				if is_move_within_board(locations[idx]):
+					if BOARD(locations[idx]) == EMPTY_SQUARE:
+						moves.append(locations[idx])
+					elif BOARD(locations[idx]).color == opponent_side:
+						moves.append(locations[idx])
+						# Done searching in this direction.
+						basis_vectors.remove_at(idx)
+						locations.remove_at(idx)
+						# Removing an element shrinks the array, push the index back by one. 
+						idx -= 1
+					else:
+						# Hit a friendly piece. 
+						basis_vectors.remove_at(idx)
+						locations.remove_at(idx)
+						# Removing an element shrinks the array, push the index back by one. 
+						idx -= 1
 				else:
-					# Hit a friendly piece. 
+					# Out of bounds.
 					basis_vectors.remove_at(idx)
 					locations.remove_at(idx)
-					# Removing an element shrinks the array, push the index back by one. 
-					idx -= 1
-			else:
-				# Out of bounds.
-				basis_vectors.remove_at(idx)
-				locations.remove_at(idx)
-			if idx+1 >= len(basis_vectors):
-				# Loop around until all baisis vectors have led to dead ends.
-				idx = 0
-			else:
-				idx += 1
+				if idx+1 >= len(basis_vectors):
+					# Loop around until all baisis vectors have led to dead ends.
+					idx = 0
+				else:
+					idx += 1
+		ChessPiece.Type.KNIGHT:
+			var knight_basis_vectors = [Vector2i(2,1), Vector2i(2,-1), Vector2i(1,2), Vector2i(1,-2)]
+			for knight_basis_vector in knight_basis_vectors:
+				move = piece.location+knight_basis_vector
+				if is_move_within_board(move) and (BOARD(move) == EMPTY_SQUARE || BOARD(move).color == opponent_side):
+					moves.append(move)
+				move = piece.location-knight_basis_vector
+				if is_move_within_board(move) and (BOARD(move) == EMPTY_SQUARE || BOARD(move).color == opponent_side):
+					moves.append(move)
+				
 		
 	return moves
-			
-			
-			
-		
-			
 func piece_clicked(piece: ChessPiece):
 	active_piece = piece
 	display_available_moves(piece)
@@ -104,7 +108,6 @@ func get_cell_center(location: Vector2)->Vector3:
 	# TODO: Fix this hack
 	var offset = abs(second_square_marker.position-first_square_marker.position).x
 	return Vector3((7-location.y)*offset,0,(7-location.x)*offset)+first_square_marker.position
-
 ## Displays available moves for the given piece by placing clickable MoveMarker's 
 ## on the chess board.
 func display_available_moves(piece: ChessPiece):
@@ -132,8 +135,7 @@ func move_piece(piece: ChessPiece, new_location: Vector2i):
 	piece.obj_ref.position.z = new_pos.z
 	# Cleanup 
 	clear_move_markers()
-	active_piece = null
-	
+	active_piece = null	
 # Setup 
 func _init():
 	for col in range(8):
@@ -186,7 +188,6 @@ func make_piece(row: int,col: int, color: ChessPiece.Side, type: ChessPiece.Type
 				if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
 					piece_clicked(piece))
 	chess_board[row][col] = piece 
-
 func CB(letter: String, num: int):
 	return chess_board[letter[0].to_ascii_buffer()[0]-65][num-1]
 func is_move_within_board(move: Vector2i):
